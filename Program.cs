b34f7c12465520a41d5347f58738f66fbf75e04d
MaviYoki <mariavitoriapossi@hotmail.com>
2023-08-16 16:51:34 -0300

    feat: method NewFile

--- Program.cs
+++ Program.cs
@@ -3,16 +3,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
         Menu();
-
     }
 
     static void Menu()
     {
-
-
-
         Console.WriteLine("What do you want to do?");
         Console.WriteLine("0- Exit");
         Console.WriteLine("1- Open file");
@@ -28,23 +23,53 @@ internal class Program
 
         var option = int.Parse(input);
 
-        switch(option)
+        switch (option)
         {
-            case 0: Console.WriteLine("Exit..."); break;
-            case 1: Console.WriteLine("Open file..");OpenFile(); break;
-            case 2: Console.WriteLine("Create new file..");NewFile(); break;
-            default: Menu(); break;
+            case 0:
+                Console.WriteLine("Exit...");
+                break;
+            case 1:
+                Console.WriteLine("Open file..");
+                OpenFile();
+                break;
+            case 2:
+                Console.WriteLine("Create new file..");
+                NewFile();
+                break;
+            default:
+                Menu();
+                break;
         }
 
     }
 
     static void OpenFile()
     {
-
     }
 
-    static void NewFile() 
+    static void NewFile()
     {
+        Console.Clear();
 
+        Console.WriteLine("     Enter your text below");
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine(" ");
+
+        var textInput = "";
+
+        do
+        {
+            textInput += Console.ReadLine();
+
+            if (string.IsNullOrEmpty(textInput))
+            {
+
+                Console.WriteLine("Inavelid input!!");
+                return;
+            }
+
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine(textInput);
     }
 }
