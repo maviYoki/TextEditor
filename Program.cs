02cbf5643e3b7ce2e1ed109c29edd41ae540e719
MaviYoki <mariavitoriapossi@hotmail.com>
2023-08-16 14:59:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Menu();
+
+    }
+
+    static void Menu()
+    {
+
+
+
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("0- Exit");
+        Console.WriteLine("1- Open file");
+        Console.WriteLine("2- Create new file");
+
+        var input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Input invalid");
+            return;
+        }
+
+        Console.WriteLine(input);
+
+
+
+    }
+}
